0468fa4768b6aeb5c8692ce22d081c0f44714217
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-02-29 09:10:15 +0530

    second commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+dataset = [
+    {'name':'Deep',
+     'age':29,
+     'city':'siliguri'},
+
+    {'name':'Ankita',
+     'age':28,
+     'city':'siliguri'},
+
+    {'name':'Krish',
+     'age':35,
+     'city':'Bangalore'},
+]
+
+df = pd.DataFrame(dataset)
+
+df.to_csv("data/data.csv",index=False)
